62ad48bfca7c28d86174e9b6e0e532706e8c8cfa
YuriiOnyskiv <49688695+YuriiOnyskiv@users.noreply.github.com>
2019-06-23 20:42:48 +0200

    Update

---
+++ Team.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Programest - Team</title>
+  <!-- adding bootstrap -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+  <link rel="stylesheet"  href="css/animate.css">
+  <script src="javascript/Jquery.js"></script>
+  <link rel="stylesheet" href="css/main.css">
+  <body style="background-color:white;">
+    <!--Header-->
+    <nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+      <div class="conteiner-fluid">
+        <div class="d-flex flex-row justify-content-between">
+            <a href="index.php" class="navbar-brand"><img src="img/logo.jpg" id="logo"></a>
+          <h1>Programest</h1>
+          <button type="button" class="navbar-toggler" data-toggle="collapse"
+          data-target="#navbarResponsive">
+          <span class="navbar-toggler-icon"></span> </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a href="index.php" class="nav-link">Main</a>
+              </li>
+              <li class="nav-item">
+                <a href="AboutUs.php" class="nav-link">About us</a>
+              </li>
+              <li class="nav-item">
+              <a href="javascript://0" onclick="slowScroll ('#more')" class="nav-link">Contacts</a>
+
+            </li>
+            <li class="nav-item">
+              <a href="Research.php" class="nav-link">Research IT</a>
+            </li>
+            <li class="nav-item">
+              <a href="Team.php" class="nav-link">Team</a>
+            </li>
+          </ul>
+
+        </div>
+      </div>
+    </div>
+  </nav>
+  <!-- Team-->
+  <br>
+<br>
+
+  <div class="container-fluid padding">
+    <div class="row text-center padding">
+      <div class="offset-lg-1 col-xs-12 col-sm-6 col-md-4" id="team" >
+
+        <h2 id="Software">Custom Software Application Development</h2>
+        <p class="lead">IT Team is your perfect application development partner when it comes to crafting a vehicle to help you reaching your goals or setting up or keeping your outpost. Whether you need to build or maintain, we have the required tools, processes, knowledge and attitude.</p>
+      </div>
+      <div class="col-lg-4">
+        <div  class="img-fluid padding"><iframe src="https://giphy.com/embed/3o7aDaA9obiRkaS7lu" width="110%" height="110%" style="position:absolute" frameBorder="0" class="gif" allowFullScreen></iframe></div>
+      </div>
+    </div>
+    </div>
+<div class="container-fluid padding" style="background-color:rgb(194, 194, 194);">
+  <div class="row text-center padding">
+    <div class="col-lg-12 my">
+      <h2 class="text-center">Custom Application Development and Maintenance</h2>
+      <h3 class="tex-center">Custom Software  Development</h3>
+      <div class="col-lg-12">
+        <p class="lead">Custom software development is the designing of software applications for a specific purpose or group of users within your organization. Such software is designed to address your needs precisely as compared to the more traditional ready-made software. Such applications are typically created just for your specific entity and is not packaged for reselling.</p>
+        <p class="lead">Customized software development entails the commissioning, development and release of a software product keeping in mind a specific need.The software is designed keeping the company’s infrastructure, branding and implementation needs in mind, implying it will be unique to that particular company.</p>
+        <p class="lead">IT Teams provides corrective, preventing and enhancing maintenance, helping the client benefit from a mature process designed to increase the adoption of internal software tools while reducing the reactive efforts to maintain the client application portfolio developed by IT Teams. You can construct a roadmap and IT Teams helps you further your goals in an organized manner. Our team works closely with your users to understand and explore the solution to its maximum capacity  and benefit from all its features.</p>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</div>
+<img src="img/team_2.png" class="img-fluid">
+<div class="conteiner-fluid padding ex">
+  <div class="row text-center ">
+    <div class="col-xs-12 col-sm-6 col-md-4">
+      <img src="img/image1.jpg" class="img-fluid">
+      <h3 class="text-center">Personalized solution</h3>
+<p class="lead">When you buy ready-to-go software solutions, it is not guaranteed that it will be a perfect fit for your organization. Custom software built as per your specifications avoids the risk of failure in implementing the solution, provided that it is developed by the right team who understand your requirements perfectly.</p>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-4">
+      <img src="img/image2.jpg" class="img-fluid">
+      <h3 class="text-center">Cost effective</h3>
+<p class="lead">The costs involved in building a custom software solution maybe high in the beginning, but they prove beneficial in the long run. Custom software does not require constant change or development to suit company requirements as the solution is unique and is not dependent on having a version that suits multiple companies and that needs to be upgraded for all users. They can be used with very little investment for a long time.</p>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-4">
+      <img src="img/image3.png" class="img-fluid">
+      <h3 class="text-center">Secure</h3>
+<p class="lead">Well known and widely used software solutions are the ones that are hacked often as they are publicly available and hackers exploit vulnerabilities and know how to compromise the security of the solution. Software developed exclusively for your organization reduces the risk of intrusions.</p>
+    </div>
+<div class="col-lg-12">
+
+<a href="index.php"><button type="button" class="btn btn-info btn-lg" style="width:95%; padding-left:5%">Home</button></a>
+</div>
+  </div>
+
+</div>
+
+<!-- Just block with nature image on the background -->
+<figure>
+  <div class="fixed-wrap">
+    <div id="fixed">
+    </div>
+  </div>
+</figure>
+<!-- footer -->
+<footer class="container-fluid" id="footer">
+  <div class="container-fluid">
+    <div class="row padding text-center">
+      <div class="col-12">
+        <h2>Our contacts</h2>
+
+      </div>
+      <div class="col-12 social padding" id="more">
+        <a href="https://twitter.com/?lang=pl" target="_blank"><i class="fab fa-twitter"></i></a>
+        <a href="https://www.instagram.com/?hl=pl" target="_blank"><i class="fab fa-instagram"></i></a>
+        <a href="https://pl-pl.facebook.com/" target="_blank"><i class="fab fa-facebook"></i></a>
+        <a href="https://www.youtube.com/" target="_blank"><i class="fab fa-youtube"></i></a>
+        <a href="https://accounts.google.com/signin/v2/identifier?passive=1209600&continue=https%3A%2F%2Faboutme.google.com%2Fu%2F0%2F%3Freferer%3Dgplus&followup=https%3A%2F%2Faboutme.google.com%2Fu%2F0%2F%3Freferer%3Dgplus&hl=pl&flowName=GlifWebSignIn&flowEntry=ServiceLogin" target="_blank"><i class="fab fa-google-plus-g"></i></a>
+<p>Tel. +48 574 770 333</p>
+<p>Email: w58996@student.wsiz.rzeszow.pl</p>
+<p>All rights reserved &copy 2019</p>
+      </div>
+
+    </div>
+
+  </div>
+
+</footer>
+<script>
+function slowScroll (id){
+  var offset = 0;
+  $('html, body').animate ({
+    scrollTop: $(id).offset ().top - offset
+  }, 1000);
+  return false;
+}
+
+</script>
+</body>
+</html>
